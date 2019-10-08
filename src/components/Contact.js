89dbd2cefa158afd761eddc98a8024e61e1d1d24
Fanny <fanny.bouffier@gmail.com>
2019-10-08 22:41:16 +0200

    fixed class issues in contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,11 +6,11 @@ const Contact = (props) => {
     return (
     <div className="Contact">
         <img className="avatar" src={props.avatar} alt={props.name}/>
-        <div class= "name">
+        <div className= "name">
             <h4>{props.name}</h4>
-            <div class="status">
-                <div class={props.online? 'status-online':'status-offline'}></div>
-                <p class="status-text">{props.online? 'Online':'Offline'}</p>
+            <div className="status">
+                <div className={props.online? 'status-online':'status-offline'}></div>
+                <p className="status-text">{props.online? 'Online':'Offline'}</p>
             </div>
         </div>
     </div>
