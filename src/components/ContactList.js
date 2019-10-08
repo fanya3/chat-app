75d00a3fa4a10daa2249545cfb6141d6de4fd199
Fanny <fanny.bouffier@gmail.com>
2019-10-08 21:24:13 +0200

    creation of contactlist.js file

---
+++ src/components/ContactList.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Contact from "./Contact";
+
+const list = [
+    {
+        avatar: "https://randomuser.me/api/portraits/med/men/75.jpg",
+        name: "Michael Adams",
+        online: 'true'
+    },
+
+    {
+        avatar: "https://randomuser.me/api/portraits/women/69.jpg",
+        name: "Felecia Schmidt",
+        online: 'false'
+    },
+
+    {
+        avatar: "https://randomuser.me/api/portraits/men/58.jpg",
+        name: "Joshua Powell",
+        online: 'true'
+    },
+
+    {
+        avatar: "https://randomuser.me/api/portraits/women/59.jpg",
+        name: "Sheila Elliott",
+        online: 'false'
+    },
+
+    {
+        avatar: "https://randomuser.me/api/portraits/men/44.jpg",
+        name: "Scott Howard",
+        online: 'false'
+    }
+];
+
+const ContactList = () => (
+    <div>
+      {list.map(item => (
+    <Contact avatar={item.avatar} name={item.name} online={item.online} />
+      )
+      )}
+    </div>
+  );
+
+
+export default ContactList;
